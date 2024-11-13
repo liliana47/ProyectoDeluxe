913c423bca9ccfe7164253bea7e165f899c1594f
AndresGrijalba <kevingr2508@gmail.com>
2024-11-13 08:14:17 -0500

    update mainwindow

--- GUI/MainWindow.xaml.cs
+++ GUI/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GUI.Pages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,14 +16,41 @@ using System.Windows.Shapes;
 
 namespace GUI
 {
-    /// <summary>
-    /// Lógica de interacción para MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void rdIngresos_Click(object sender, RoutedEventArgs e)
+        {
+            //frameContent.Navigate(new Factura());
+        }
+
+        private void rdIngresos_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void rdProductos_Click(object sender, RoutedEventArgs e)
+        {
+            //frameContent.Navigate(new Productos());
+        }
+
+        private void rdProductos_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
         }
     }
 }
