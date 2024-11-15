917f08305ef9da450be0239905b256d9930953f0
AndresGrijalba <kevingr2508@gmail.com>
2024-11-15 12:47:38 -0500

    update productosdal

--- ENTITY/Cliente.cs
+++ ENTITY/Cliente.cs
@@ -1,16 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ENTITY
 {
-    public class Cliente : Persona
+    public class Cliente 
     {
-        public Cliente(string nombre, string apellido, string tipoDocumento, int numeroDocumento, int telefono, string correoElectronico)
-            : base(nombre, apellido, tipoDocumento, numeroDocumento, telefono, correoElectronico)
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string TipoDocumento { get; set; }
+        public int NumeroDocumento { get; set; }
+        public int Telefono { get; set; }
+        public string CorreoElectronico { get; set; }
+
+        public Cliente()
         {
+
+        }
+
+        public Cliente(string nombre, string apellido, string tipoDocumento, int numeroDocumento, int telefono, string correoElectronico)
+        {
+            Nombre = nombre;
+            Apellido = apellido;
+            TipoDocumento = tipoDocumento;
+            NumeroDocumento = numeroDocumento;
+            Telefono = telefono;
+            CorreoElectronico = correoElectronico;
         }
     }
 }
--- GUI/AgregarPWindow.xaml.cs
+++ GUI/AgregarPWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using BLL;
 
 namespace GUI
 {
@@ -19,14 +20,22 @@ namespace GUI
     /// </summary>
     public partial class AgregarPWindow : Window
     {
+        private ProductoBLL productoBLL = new ProductoBLL(); 
+
         public AgregarPWindow()
         {
             InitializeComponent();
         }
 
-        public void AgregarProducto (object sender, RoutedEventArgs e)
+        public void btnAgregar_Click(object sender, RoutedEventArgs e)
         {
+            string nombre = txtNombre.Text;
+            string descripcion = txtDescripcion.Text;
+            int cantidad = Convert.ToInt16(txtCantidad.Text);
+            double precio = Convert.ToDouble(txtPrecioUnitario.Text);
+            double impuesto = Convert.ToDouble(txtImpuesto.Text);
 
+            productoBLL.AgregarProducto(nombre, descripcion,cantidad, precio);
         }
 
         private void btnClose_Click(object sender, RoutedEventArgs e)
